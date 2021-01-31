50bf15f18e520f12c818fe96464d09cf4c4f53e1
André L F S Bacci <alfsb@users.noreply.github.com>
2021-01-30 23:46:21 -0300

    New diff text diff emphasis.

--- revcheckdiff.php
+++ revcheckdiff.php
@@ -1,41 +1,48 @@
 <?php
 
-require './Diff.php';
+require './simplediff.php';
 
 $f  = $_GET['f'];
 $h1 = $_GET['h1'];
 $h2 = $_GET['h2'];
-$m = isset( $_GET['mode'] ) ? $_GET['mode'] : 'text';
+$mode = isset( $_GET['mode'] ) ? $_GET['mode'] : 'text';
 
-$url1 = "http://git.php.net/?p=doc/en.git;a=blob_plain;f=$f;hb=$h1";
-$url2 = "http://git.php.net/?p=doc/en.git;a=blob_plain;f=$f;hb=$h2";
+//$f='appendices/about.xml';
+//$h1='bcd51bdf1d04d8b18ed058f8de03cfa2a4c2fb9b';
+//$h2='86e6094e86b84a51d00ab217ac50ce8dde33d82a';
+//$mode = "html";
 
-$contents1 = file_get_contents( $url1 );
-$contents2 = file_get_contents( $url2 );
+$old = "http://git.php.net/?p=doc/en.git;a=blob_plain;f=$f;hb=$h1";
+$new = "http://git.php.net/?p=doc/en.git;a=blob_plain;f=$f;hb=$h2";
 
-$diff = Diff::compare( $contents1 , $contents2 );
+$old = file_get_contents( $old );
+$new = file_get_contents( $new );
 
-if ( $m == 'html' )
+$old = splitRaw( $old );
+$new = splitRaw( $new );
+$diff = diff( $old , $new );
+
+unset( $old );
+unset( $new );
+
+switch ( $mode )
 {
-    header( "Content-Type: text/html" );
-    print <<<HTML
-<html>
-<head>
-<style>
-.diff td { vertical-align: top; white-space: pre; white-space: pre-wrap; font-family: monospace; }
-.diffDeleted { border: 1px solid red; background: MistyRose; }
-.diffInserted { border: 1px solid green; background: HoneyDew; }
-.diffUnmodified { color: DimGray; }
-</style>
-</head>
-<body>
-HTML;
-    print Diff::toTable( $diff );
-    print '</body></html>';
-}
-else
-{
-    header( "Content-Type: text/plain" );
-    print Diff::toString( $diff );
+    case "html":
+    case "html2":
+        include "revcheckdiffhtml.php";
+        break;
+    default:
+        include "revcheckdifftext.php";
+        break;
 }
 
+exit;
+
+function splitRaw( $text )
+{
+    if ( strpos( $text , "\r\n" ) !== false )
+        return explode( "\r\n" , $text );
+    if ( strpos( $text , "\n" ) !== false )
+        return explode( "\n" , $text );
+    return explode( "\r" , $text );
+}
